abb2c1486ce1bd473a5ea0a0bcfb164c8378d6e3
fkmac <fkmac@DESKTOP-6TKACUD>
2016-10-04 23:16:26 -0300

    Tela Login Funcionario

--- Savar_git/Resources/Resource.Designer.cs
+++ Savar_git/Resources/Resource.Designer.cs
@@ -331,11 +331,11 @@ namespace Savar_git
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f07000f
-			public const int SysLog = 2131165199;
+			// aapt resource value: 0x7f07001a
+			public const int SysLog = 2131165210;
 			
-			// aapt resource value: 0x7f07000e
-			public const int TesteButton = 2131165198;
+			// aapt resource value: 0x7f070019
+			public const int TesteButton = 2131165209;
 			
 			// aapt resource value: 0x7f070005
 			public const int adjust_height = 2131165189;
@@ -346,14 +346,20 @@ namespace Savar_git
 			// aapt resource value: 0x7f07000a
 			public const int auto = 2131165194;
 			
+			// aapt resource value: 0x7f070018
+			public const int button1 = 2131165208;
+			
 			// aapt resource value: 0x7f07000b
 			public const int dark = 2131165195;
 			
-			// aapt resource value: 0x7f07000d
-			public const int editText1 = 2131165197;
+			// aapt resource value: 0x7f070012
+			public const int editText1 = 2131165202;
 			
-			// aapt resource value: 0x7f070011
-			public const int gridLayout1 = 2131165201;
+			// aapt resource value: 0x7f070016
+			public const int editText2 = 2131165206;
+			
+			// aapt resource value: 0x7f07000d
+			public const int gridLayout1 = 2131165197;
 			
 			// aapt resource value: 0x7f070000
 			public const int hybrid = 2131165184;
@@ -364,8 +370,23 @@ namespace Savar_git
 			// aapt resource value: 0x7f07000c
 			public const int light = 2131165196;
 			
-			// aapt resource value: 0x7f070010
-			public const int mapFrag = 2131165200;
+			// aapt resource value: 0x7f07000f
+			public const int linearLayout1 = 2131165199;
+			
+			// aapt resource value: 0x7f070011
+			public const int linearLayout2 = 2131165201;
+			
+			// aapt resource value: 0x7f070013
+			public const int linearLayout3 = 2131165203;
+			
+			// aapt resource value: 0x7f070015
+			public const int linearLayout4 = 2131165205;
+			
+			// aapt resource value: 0x7f070017
+			public const int linearLayout5 = 2131165207;
+			
+			// aapt resource value: 0x7f07001b
+			public const int mapFrag = 2131165211;
 			
 			// aapt resource value: 0x7f070001
 			public const int none = 2131165185;
@@ -376,11 +397,20 @@ namespace Savar_git
 			// aapt resource value: 0x7f070003
 			public const int satellite = 2131165187;
 			
+			// aapt resource value: 0x7f07000e
+			public const int space1 = 2131165198;
+			
 			// aapt resource value: 0x7f070008
 			public const int standard = 2131165192;
 			
 			// aapt resource value: 0x7f070004
 			public const int terrain = 2131165188;
+			
+			// aapt resource value: 0x7f070010
+			public const int textView1 = 2131165200;
+			
+			// aapt resource value: 0x7f070014
+			public const int textView2 = 2131165204;
 			
 			// aapt resource value: 0x7f070009
 			public const int wide = 2131165193;
@@ -415,16 +445,16 @@ namespace Savar_git
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int layout1 = 2130903040;
+			public const int funcionario = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Login = 2130903041;
+			public const int layout1 = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int Main = 2130903042;
+			public const int Login = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int motorista = 2130903043;
+			public const int Main = 2130903043;
 			
 			static Layout()
 			{
