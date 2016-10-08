332aba119f374ea5e5586cd245f23e5926dc4551
AleCuchi <ale.cuchi.p@gmail.com>
2016-10-07 21:46:32 -0300

    Insert Funcionando, meio caminho no update.

--- Savar_git/DataResource/OnibusClass.cs
+++ Savar_git/DataResource/OnibusClass.cs
@@ -18,18 +18,71 @@ namespace Savar_git
     {
         private string ConnectionString = "";
 
-        OnibusClass(string StringConect)
+        public OnibusClass(string StringConect)
         {
             ConnectionString = StringConect;
         }
-        public bool InputOnibus(int nNunOnibus, string cPlaca, double PosX, double PosY, double PosZ, string cRota )
+/**************************************
+ * Função InputOnibus - Função para realizar inclusão de novo ônibus
+ * Parâmetros 
+ *      cNunOnibus - Variavel String com o número identificado  do Onibus 
+ *      cPlaca - Placa do Veículo 
+ *      cRota - Identificador de uma rota que o ônibus vai fazer.
+ **************************************/
+        public void InputOnibus(string  cNunOnibus , string cPlaca , string cRota="" )
         {
-            bool lRet = false;
+            string lRet = "";
+            dbMngmt Database = new dbMngmt();
+            string cQuery = "";
+            MySqlDataAdapter OnibusData = new MySqlDataAdapter();
+            MySqlCommand Command = null;
 
-            
-            return lRet;
+
+            cQuery += "USE Savar; ";
+            cQuery += "INSERT INTO Savar.onibus (numero_onibus, placa ";
+            if(cRota != "")
+            {
+                cQuery += ", rota) ";
+            }
+            else
+            {
+                cQuery += ") ";
+            }
+            cQuery += " VALUES (" +cNunOnibus+ ",  '"+ cPlaca+"' ";
+            if (cRota != "")
+            {
+                cQuery += ", "+cRota+" ) ;";
+            }
+            else
+            {
+                cQuery += ") ;";
+            }
+
+            Command = new MySqlCommand(cQuery, Database.Database);
+
+            try
+            {
+                if (Database.ConectionTest())
+                {
+                    Command.ExecuteNonQuery();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                lRet = ex.ToString();
+            }
         }
-        public void SelectOnibus(int nNumero = 0, string cPlaca= "", string cRota="")
+/*********************************
+ * Função SelectOnibus()
+ * Parametros : 
+ *      nNumero = Número do Onibus.
+ *      cPlaca  = String com a placa do Veículo
+ *      cRota   = Rota De ônibus
+ *  Deve ser passado o parametro nNumero e cPlaca, ou Somemente o cRota, 
+ *  caso quera verificar os ônibus de uma determinada rota.
+ *  
+ * ******************************/
+        public DataTable SelectOnibus(int nNumero = 0, string cPlaca= "", string cRota="")
         {
             dbMngmt Database = new dbMngmt();
             DataTable Onibus = new DataTable();
@@ -64,12 +117,56 @@ namespace Savar_git
             {
                 
             }
-            
-            
+            return Onibus;
         }
-        public bool UpdateOnibus()
+
+
+        public bool UpdateOnibus(string cNumOnibus, string cPlaca, double PosX=0, double PosY=0, double PosZ=0, string cNewNumOnibus="", string cNewPlaca="")
         {
             bool lRet = false;
+            string cQuery = "";
+            dbMngmt Database = new dbMngmt();
+            MySqlCommand Command;
+
+            cQuery += "USE Savar; ";
+            cQuery += "UPDATE Savar.onibus (";
+            if(cNewNumOnibus != "")
+            {
+                cQuery += " numero_onibus ";
+            }
+            if(cNewPlaca != "")
+            {
+                cQuery += " ,placa ";
+            }
+            if(PosX != 0 || PosY != 0 || PosZ != 0)
+            {
+                cQuery += " ,x,y,z  ";
+            }
+            cQuery += " ) Values ( ";
+            if (cNewNumOnibus != "")
+            {
+                cQuery += " " + cNewNumOnibus + " , ";
+            }
+            if(cNewPlaca != "")
+            {
+                cQuery += " " + cNewPlaca + " , ";
+            }
+            if(PosX != 0 || PosY != 0 || PosZ != 0)
+            {
+                cQuery += " "+PosX.ToString()+" , "+PosY.ToString()+" , "+PosZ.ToString()+"   ";
+            }
+            cQuery += " ) WHERE numero_onibus = "+ cNumOnibus +" AND placa = "+ cPlaca +" ; ";
+            Command = new MySqlCommand(cQuery, Database.Database);
+            try
+            {
+                if (Database.ConectionTest())
+                {
+                    Command.
+                }
+            }catch(MySqlException ex)
+            {
+
+            }
             return lRet;
         }
         public bool DeleteOnibus()
